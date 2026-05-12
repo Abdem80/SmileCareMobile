5d626322e907bdfdd72ba45862e671473524fe5f
Prénom Nom <votre.email@cegepsherbrooke.qc.ca>
2026-05-12 10:17:03 -0400

    Main activity coded

--- app/src/main/java/com/example/smilecaremobile/MainActivity.java
+++ app/src/main/java/com/example/smilecaremobile/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.smilecaremobile;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,10 +19,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.menu);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_options, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
